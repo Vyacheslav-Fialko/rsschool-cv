afde81eb2c06aaff716a02d3c619aa8082686f17
Vyacheslav-Fialko <vyacheslav1@ukr.net>
2021-09-20 18:27:04 +0300

    feat: added functional close menu after link click

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-document.querySelector('.burger').addEventListener('click', function(){
-  document.querySelector('.burger span').classList.toggle('active');
-  document.querySelector('.nav-list').classList.toggle('active');
-})
+const burger = document.querySelector('.burger');
+const burgerSpan = burger.querySelector('span');
+const navList = document.querySelector('.nav-list');
+const navItems = navList.querySelectorAll('.nav-item');
+
+burger.addEventListener('click', toggleBurger);
+
+navItems.forEach(link => {
+
+  link.addEventListener('click', () => {
+    if (navList.classList.contains('active')) {
+      toggleBurger();
+    }
+  });
+
+});
+
+function toggleBurger() {
+  burgerSpan.classList.toggle('active');
+  navList.classList.toggle('active');
+}
